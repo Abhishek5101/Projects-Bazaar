af46184928273f33b70bfb88c6185f9543d051e7
Abhishek5101 <miabhishekkulkarni@gmail.com>
2019-10-15 23:02:45 -0700

    cards at center

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template, url_for, flash
+from flask import Flask, render_template, url_for, flash, request, redirect
 from pymongo import MongoClient
 
 cluster = MongoClient("mongodb+srv://abhi:intensive472@intensive-lsw6x.gcp.mongodb.net/test?retryWrites=true&w=majority")
-test_db = cluster["test_db"]
-test_collection = test_db["test_collection"]
+db = cluster["Database"]
+projects = db["projects"]
 
-post = {"name": "tim"}
+app = Flask(__name__)
+@app.route('/')
+def home():
+	return render_template('home.html', projects=projects.find())
 
-test_collection.insert_one(post)
+
+@app.route('/form', methods=["GET", "POST"])
+def form():
+	req = request.form
+	if request.method == "POST":
+		project = {
+			"name": req["name"],
+			"description": req["description"],
+			"img_url": req["url"],
+			"githuburl": req["githuburl"]
+		}
+		projects.insert_one(project)
+	elif request.method == "GET":
+		return render_template('/project_form.html')
+	return redirect('/')
+
+
+@app.route('/home', methods=["GET"])
+def homeie():
+	return render_template('home.html', projects=projects.find())
